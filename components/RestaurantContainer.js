dc5c201b3f207fa02ffcfdf97958f6bd1c2261bb
hsdamle <hsdamle@gmail.com>
2023-11-16 13:05:58 +0530

    episode05

--- components/RestaurantContainer.js
+++ components/RestaurantContainer.js
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import { RestaurantData } from "../utils/RestaurantData";
 import RestaurantCard from "./RestaurantCard";
 
 export default function RestaurantContainer() {
+  const [resList, setResList] = useState(RestaurantData);
+
+  const filterHighestrated = () => {
+    const data = resList.filter(ele => {
+        return ele.info.avgRating > 4.5
+    });
+    setResList(data);
+  };
+
   return (
-    <div className="res-container">
-      {RestaurantData.map((res) => {
-        return <RestaurantCard resData={res} />;
-      })}
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={() => filterHighestrated()}>Top Rated Restaurants</button>
+      </div>
+      <div className="res-container">
+        {resList.map((res) => {
+          return <RestaurantCard key={res?.info?.id} resData={res} />;
+        })}
+      </div>
     </div>
   );
 }
