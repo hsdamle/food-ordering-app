2f5691dfdb8040943bdcf3005646be7e3d5ad397
hsdamle <hsdamle@gmail.com>
2023-11-28 22:57:41 +0530

    episode08

--- components/About.js
+++ components/About.js
@@ -1,7 +1,22 @@
-export default function About() {
-    return (
-        <div>
-            <h1>About Us Page</h1>
-        </div>
-    )
+import React from "react";
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log("Constructor Called");
+    }
+
+    componentDidMount() {
+        console.log("Component did mount called");
+    }
+
+    render() {
+        console.log("render called");
+        return (
+            <div>
+                <h1>About Us page</h1>
+            </div>
+        )
+    }
 }
+
+export default User;
